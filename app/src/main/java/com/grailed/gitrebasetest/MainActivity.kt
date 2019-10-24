f65ef6912df54a20b87a14139c3174025b3185ac
XianxianChen <xianxianchen@ac.c4q.nyc>
2019-10-24 17:32:32 -0400

    F1

--- app/src/main/java/com/grailed/gitrebasetest/MainActivity.kt
+++ app/src/main/java/com/grailed/gitrebasetest/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         Log.d("D1", "D1")
-   
+        Log.d("F1", "F1")
+      
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
