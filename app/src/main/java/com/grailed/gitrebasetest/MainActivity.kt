4f9f7ee94f0c318ee4ae040c841daae000dc7f70
XianxianChen <xianxianchen@ac.c4q.nyc>
2019-10-24 17:31:52 -0400

    D1

--- app/src/main/java/com/grailed/gitrebasetest/MainActivity.kt
+++ app/src/main/java/com/grailed/gitrebasetest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.grailed.gitrebasetest
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        Log.d("D1", "D1")
+   
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
